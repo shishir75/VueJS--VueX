04bc404fa5b3ac3bf37f801ce86c8964a90a6992
shishir75 <shishir.srdr16@gmail.com>
2020-04-20 22:54:44 +0600

    Simple Increment/Decrement

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <div id="app">
-
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1>VueX</h1>
+        <app-result :counter="counter"></app-result>
+        <hr>
+        <app-counter @updated="counter += $event"></app-counter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Counter from "./components/Counter";
+import Result from "./components/Result";
 
 export default {
   name: 'App',
-  
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  components: {
+    appCounter: Counter,
+    appResult: Result,
+  }
+
 }
 </script>
 
---
+++ src/components/Result.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Counter is : {{ counter }}</p>
+</template>
+
+<script>
+    export default {
+        name: "Result",
+        props: ['counter']
+    }
+</script>
+
+<style scoped>
+
+</style>
